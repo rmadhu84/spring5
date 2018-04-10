d6fe4b91887c35bf29395988c459567b5c15780f
rmadhu84 <r.madhu.84@gmail.com>
2018-04-10 05:36:06 -0400

    Added Getters and Setters

--- spring5webapp/src/main/java/com/springframework/spring5webapp/models/Contact.java
+++ spring5webapp/src/main/java/com/springframework/spring5webapp/models/Contact.java
@@ -23,4 +23,46 @@ public class Contact {
 		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the phone
+	 */
+	public List<Phone> getPhone() {
+		return phone;
+	}
+
+	/**
+	 * @param phone the phone to set
+	 */
+	public void setPhone(List<Phone> phone) {
+		this.phone = phone;
+	}
+
 }
--- spring5webapp/src/main/java/com/springframework/spring5webapp/models/Phone.java
+++ spring5webapp/src/main/java/com/springframework/spring5webapp/models/Phone.java
@@ -20,4 +20,46 @@ public class Phone {
 		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the type
+	 */
+	public String getType() {
+		return type;
+	}
+
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	/**
+	 * @return the no
+	 */
+	public String getNo() {
+		return no;
+	}
+
+	/**
+	 * @param no the no to set
+	 */
+	public void setNo(String no) {
+		this.no = no;
+	}
+
 }
