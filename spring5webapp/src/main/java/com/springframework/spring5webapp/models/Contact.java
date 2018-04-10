880cab423909936a710f98e7317318a99b476f85
ramachandranm1 <ramachandranm1@T60844XZWY52.som.ad.state.mi.us>
2018-04-10 16:53:07 -0400

    Added HashCode, equals, toString methods and parameterized constructors.

--- spring5webapp/src/main/java/com/springframework/spring5webapp/models/Contact.java
+++ spring5webapp/src/main/java/com/springframework/spring5webapp/models/Contact.java
@@ -73,6 +73,74 @@ public class Contact {
 		this.phone = phone;
 	}
 
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((phone == null) ? 0 : phone.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Contact other = (Contact) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (phone == null) {
+			if (other.phone != null)
+				return false;
+		} else if (!phone.equals(other.phone))
+			return false;
+		return true;
+	}
+
+
+	/**
+	 * @param email
+	 * @param phone
+	 */
+	public Contact(String email, Set<Phone> phone) {
+		this.email = email;
+		this.phone = phone;
+	}
+
+	/**
+	 * @param email
+	 */
+	public Contact(String email) {
+		this.email = email;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Contact [id=" + id + ", email=" + email + ", phone=" + phone + "]";
+	}
+
 
 
 
--- spring5webapp/src/main/java/com/springframework/spring5webapp/models/Phone.java
+++ spring5webapp/src/main/java/com/springframework/spring5webapp/models/Phone.java
@@ -76,4 +76,72 @@ public class Phone {
 		this.no = no;
 	}
 
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Phone [id=" + id + ", type=" + type + ", no=" + no + ", contact=" + contact + "]";
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((contact == null) ? 0 : contact.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((no == null) ? 0 : no.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Phone other = (Phone) obj;
+		if (contact == null) {
+			if (other.contact != null)
+				return false;
+		} else if (!contact.equals(other.contact))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (no == null) {
+			if (other.no != null)
+				return false;
+		} else if (!no.equals(other.no))
+			return false;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		return true;
+	}
+
+	/**
+	 * @param type
+	 * @param no
+	 * @param contact
+	 */
+	public Phone(String type, String no, Contact contact) {
+		this.type = type;
+		this.no = no;
+		this.contact = contact;
+	}
+
 }
