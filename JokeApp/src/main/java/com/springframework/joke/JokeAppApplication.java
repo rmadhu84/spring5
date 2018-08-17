065ae40d7aa50da9ba8795c968fc5795c4df63e6
ramachandranm1 <ramachandranm1@T60844XZWY52.som.ad.state.mi.us>
2018-08-17 09:08:23 -0400

    Joke App - XML Bean configuration

--- JokeApp/src/main/java/com/springframework/joke/JokeAppApplication.java
+++ JokeApp/src/main/java/com/springframework/joke/JokeAppApplication.java
@@ -2,8 +2,10 @@ package com.springframework.joke;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuckConfiguration.xml") //XML based Bean Configuration.
 public class JokeAppApplication {
 
 	public static void main(String[] args) {
--- JokeApp/src/main/java/com/springframework/joke/config/ChuckConfiguration.java
+++ JokeApp/src/main/java/com/springframework/joke/config/ChuckConfiguration.java
@@ -10,12 +10,12 @@ import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 
 /**
  * @author Madhu
- *
+ * Java-Based Bean Configuration
  */
-@Configuration
+//@Configuration
 public class ChuckConfiguration {
 
-	@Bean
+	////@Bean
 	public ChuckNorrisQuotes chuckNorrisQuotes() {
 		return new ChuckNorrisQuotes();
 	}
