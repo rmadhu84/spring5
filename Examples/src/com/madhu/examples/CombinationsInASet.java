5c9c002daff7d891117737b735c15a48e6e49354
rmadhu84 <r.madhu.84@gmail.com>
2019-07-16 07:04:06 -0400

    More examples

--- Examples/src/com/madhu/examples/CombinationsInASet.java
+++ Examples/src/com/madhu/examples/CombinationsInASet.java
@@ -3,6 +3,8 @@
  */
 package com.madhu.examples;
 
+import java.util.Arrays;
+
 /**
  * @author Madhu
  *
@@ -12,18 +14,49 @@ public class CombinationsInASet {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[] arr = {1,2,3};
-		for(int i = 0; i<arr.length;i++) {
-			for(int j = i+1;j<arr.length;j++) {
-				if(j>arr.length)
-					j=0;
-				else if(j==i)
-					break;
-				System.out.println(arr[i]+","x);
-			}
+	// TODO Auto-generated method stub
+	static void combinationUtil(int arr[], int data[], int start, int end, int index, int r) {
+		// Current combination is ready to be printed, print it
+		if (index == r) {
+			for (int j = 0; j < r; j++)
+				System.out.print(data[j] + " ");
+			System.out.println("");
+			return;
+		}
+
+		// replace index with all possible elements. The condition
+		// "end-i+1 >= r-index" makes sure that including one element
+		// at index will make a combination with remaining elements
+		// at remaining positions
+		for (int i = start; i <= end && end - i + 1 >= r - index; i++) {
+			data[index] = arr[i];
+//			printArray(data);
+			System.out.println(String.format("i=%d, start=%d, end=%d, r=%d, index=%d",i, start, end, r, index));
+			combinationUtil(arr, data, i + 1, end, index + 1, r);
 		}
 	}
+
+	// The main function that prints all combinations of size r
+	// in arr[] of size n. This function mainly uses combinationUtil()
+	static void printCombination(int arr[], int n, int r) {
+		// A temporary array to store all combination one by one
+		int data[] = new int[r];
+
+		// Print all combination using temprary array 'data[]'
+		combinationUtil(arr, data, 0, n - 1, 0, r);
+	}
+
+	static void printArray(int[] data) {
+		System.out.println(Arrays.toString(data));
+	}
+	/* Driver function to check for above function */
+	public static void main(String[] args) {
+		int arr[] = { 1, 2, 3, 4, 5 };
+		int r = 3;
+		int n = arr.length;
+		printCombination(arr, n, r);
+	}
+
+	/* This code is contributed by Devesh Agrawal */
 
 }
