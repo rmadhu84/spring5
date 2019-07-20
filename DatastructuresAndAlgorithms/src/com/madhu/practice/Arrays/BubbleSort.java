b1bac0913be8334c521bfcf4ddf1fb35de8667e3
rmadhu84 <r.madhu.84@gmail.com>
2019-07-20 11:50:25 -0400

    Cleaned up Bubble Sort

--- DatastructuresAndAlgorithms/src/com/madhu/practice/Arrays/BubbleSort.java
+++ DatastructuresAndAlgorithms/src/com/madhu/practice/Arrays/BubbleSort.java
@@ -3,30 +3,39 @@ package com.madhu.practice.Arrays;
 import java.util.Arrays;
 
 public class BubbleSort {
+	static boolean swapped = false;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] inputArr = { 1, 6, 3, 2, 5 };
-
+		int[] inputArr = {26,52,35,11,10};
+		
+		System.out.println("Input :" + Arrays.toString(inputArr));
+		
 		int unsortedPartionindex = inputArr.length - 1;
 
-		for (int a = 0; a <= unsortedPartionindex; a++) {
-			for (int i = 0; i <= unsortedPartionindex - 1; i++) {
-				System.out.println(Arrays.toString(inputArr));
+		for (;unsortedPartionindex > 0; unsortedPartionindex--) {
+			swapped = false;
+			for (int i = 0; i < unsortedPartionindex; i++) {
+				//System.out.println(Arrays.toString(inputArr));
 				if (inputArr[i] > inputArr[i + 1]) {
 					inputArr = swap(inputArr, i, i + 1);
 				}
-				if (i == unsortedPartionindex)
-					unsortedPartionindex--;
 			}
-			System.out.println(Arrays.toString(inputArr));
+			
+			//System.out.println(String.format("Swapped : %b", swapped));
+//			System.out.println(Arrays.toString(inputArr));
+			if(!swapped)
+				break;
 		}
+	
+		System.out.println("Sorted Array: " + Arrays.toString(inputArr));
 	}
 
 	private static int[] swap(int[] inputArr, int i, int j) {
 		int temp = inputArr[j];
 		inputArr[j] = inputArr[i];
 		inputArr[i] = temp;
+		swapped = true;
 		return inputArr;
 	}
 
